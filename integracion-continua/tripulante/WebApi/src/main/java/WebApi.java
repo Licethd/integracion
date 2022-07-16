436288e735e3ff7d74bb1164f30b216b168a2e55
Licethd <liceth.dfe@gmail.com>
2022-07-15 21:31:16 -0400

    f5

---
+++ integracion-continua/tripulante/WebApi/src/main/java/WebApi.java
@@ -0,0 +1,16 @@
+
+import fourteam.http.Rest;
+import Controllers.TripulanteController;
+
+public class WebApi {
+    public static void AddControllers() {
+        Rest.addController(TripulanteController.class);
+
+        Rest.start(8080);
+        Rest.createSwagger();
+    }
+	public static void AddInfraestructure() {
+		// TODO Auto-generated method stub
+
+	}
+}
